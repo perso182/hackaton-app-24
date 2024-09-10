24d266c47c409b8b8e80b1a374998f237b6e01d7
Daniel Grünler <daniel.grunler@omegapoint.se>
2024-09-10 10:52:09 -0700

    add stranger things get function

--- application/fetch_quote.py
+++ application/fetch_quote.py
@@ -5,3 +5,9 @@ def get_bb_quote() -> QuoteContext:
     r = requests.get('https://api.breakingbadquotes.xyz/v1/quotes').json()[0]
     q = QuoteContext(r['quote'], r['author'], 'Breaking bad')
     return q 
+
+def get_stranger_things_quote() -> QuoteContext:
+    uri = 'https://strangerthings-quotes.vercel.app/api/quotes'
+    r = requests.get(uri).json()[0]
+    q = QuoteContext(r['quote'], r['author'], 'Stranger Things')
+    return q
