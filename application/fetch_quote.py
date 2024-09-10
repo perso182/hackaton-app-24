148491ac2e388e717982452d320846d6967a5d05
Joakim Lundström <joakim.lundstrom@omegapoint.se>
2024-09-10 10:53:36 +0200

    Fetch GoT quote

--- application/fetch_quote.py
+++ application/fetch_quote.py
@@ -5,3 +5,9 @@ def get_bb_quote() -> QuoteContext:
     r = requests.get('https://api.breakingbadquotes.xyz/v1/quotes').json()[0]
     q = QuoteContext(r['quote'], r['author'], 'Breaking bad')
     return q 
+
+
+def get_got_quote() -> QuoteContext:
+    r = requests.get('https://api.gameofthronesquotes.xyz/v1/random').json()
+    q = QuoteContext(r['sentence'], r['character']['name'] + " of " + r['character']['house']['name'], 'Game of Thrones')
+    return q
