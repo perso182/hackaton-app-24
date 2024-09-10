cc6673b466c846ca40de8aac3f41553be628e592
Daniel Grünler <daniel.grunler@omegapoint.se>
2024-09-10 11:13:43 -0700

    get random quote in main

--- application/main.py
+++ application/main.py
@@ -1,9 +1,10 @@
 import requests
 from fetch_quote import *
 from QuoteContext import *
-q = get_bb_quote()
+q = get_quote()
 
 answer = input("Gissa från vilken serie detta citat är: \n" + "\"" + q.quote + "\"\n")
+
 
 if answer.lower().strip() == q.source.lower().strip():
     print("Rätt svar!, Author: " + q.author)
