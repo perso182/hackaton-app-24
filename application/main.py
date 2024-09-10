7deac0084910710c756bb9467437310b6a21643c
Daniel Grünler <daniel.grunler@omegapoint.se>
2024-09-10 11:29:47 -0700

    add multiple choice option to terminal

--- application/main.py
+++ application/main.py
@@ -3,11 +3,33 @@ from fetch_quote import *
 from QuoteContext import *
 q = get_quote()
 
-answer = input("Gissa från vilken serie detta citat är: \n" + "\"" + q.quote + "\"\n")
+print(q.source)
+
+answer = input("Gissa från vilken serie detta citat är: \n" + "\"" + q.quote + "\"\n" + "1: Breaking Bad\n 2: Stranger Things \n3: Game of Thrones. \n")
+
+correct_answer = q.source
+
+def print_output(author, truth_value):
+    if(truth_value == True):
+        print("Rätt svar!, Author: " + author)
+    else:
+        print("Fel svar!")
 
 
-if answer.lower().strip() == q.source.lower().strip():
-    print("Rätt svar!, Author: " + q.author)
-else:
-    print("Fel svar!")
+match answer.lower().strip():
+    case "1": 
+        truth_value = (correct_answer.lower() == "Breaking Bad".lower()) 
+        print_output(q.author, truth_value)
+    case "2": 
+        truth_value = correct_answer.lower() == "Stranger Things".lower() 
+        print_output(q.author, truth_value)    
+    case "3": 
+        truth_value = correct_answer.lower() == "Game of Thrones".lower()
+        print_output(q.author, truth_value)
+    case _:
+        print("Felaktig input.")
+#if answer.lower().strip() == q.source.lower().strip():
+#    print("Rätt svar!, Author: " + q.author)
+#else:
+#    print("Fel svar!")
 
