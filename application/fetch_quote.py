024d80224915f612de863c1d0b13584e5d129778
Per Sondell <per.sondell@omegapoint.se>
2024-09-10 11:07:50 +0200

    added random quote

--- application/fetch_quote.py
+++ application/fetch_quote.py
@@ -1,7 +1,12 @@
 from QuoteContext import *
 import requests
+import random
 
 def get_bb_quote() -> QuoteContext:
     r = requests.get('https://api.breakingbadquotes.xyz/v1/quotes').json()[0]
     q = QuoteContext(r['quote'], r['author'], 'Breaking bad')
     return q 
+
+def get_quote() -> QuoteContext:
+    quote_list = [get_bb_quote]
+    return random.choice(quote_list)()
