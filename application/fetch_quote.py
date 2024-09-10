a5398f17cb6d1a52aa9c51143ecc66866457c7fd
Per Sondell <per.sondell@omegapoint.se>
2024-09-10 11:34:11 +0200

    added missing return

--- application/fetch_quote.py
+++ application/fetch_quote.py
@@ -14,6 +14,7 @@ def get_quote() -> QuoteContext:
 def get_got_quote() -> QuoteContext:
     r = requests.get('https://api.gameofthronesquotes.xyz/v1/random').json()
     q = QuoteContext(r['sentence'], r['character']['name'] + " of " + r['character']['house']['name'], 'Game of Thrones')
+    return q
     
 def get_stranger_things_quote() -> QuoteContext:
     uri = 'https://strangerthings-quotes.vercel.app/api/quotes'
